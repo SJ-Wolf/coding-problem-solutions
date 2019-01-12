5f4a50aaa433b4d89329076829028fcd6f907364
kyle1 <kyle1940@gmail.com>
2019-01-11 22:53:59 -0800

    Unique Paths II

---
+++ Problem 063 - Unique Paths II/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Problem_063___Unique_Paths_II
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+//            Console.WriteLine(new Solution().UniquePathsWithObstacles(new int[,] {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}}));
+            Console.WriteLine(new Solution().UniquePathsWithObstacles(new int[,] {{0, 0, 0}, {0, 0, 0}, {0, 0, 1}}));
+//            Console.WriteLine(new Solution().UniquePathsWithObstacles(new int[,] {{0}, {1}}));
+        }
+    }
+
+    public class Solution
+    {
+        private const int Empty = -1;
+        private const int Full = -2;
+
+        public int UniquePathsWithObstacles(int[,] obstacleGrid)
+        {
+            for (var row = 0; row < obstacleGrid.GetLength(0); row++)
+            {
+                for (var col = 0; col < obstacleGrid.GetLength(1); col++)
+                {
+                    if (obstacleGrid[row, col] == 0)
+                        obstacleGrid[row, col] = Empty;
+                    else
+                        obstacleGrid[row, col] = Full;
+                }
+            }
+
+            var output = GetLatticePaths(obstacleGrid, 0, 0);
+
+            for (var row = 0; row < obstacleGrid.GetLength(0); row++)
+            {
+                for (var col = 0; col < obstacleGrid.GetLength(1); col++)
+                {
+                    var elem = obstacleGrid[row, col];
+                    var c = elem.ToString();
+                    if (elem == Empty)
+                        c = "e";
+                    else if (elem == Full)
+                        c = "F";
+                    Console.Write(c + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            return output;
+        }
+
+        public static int GetLatticePaths(int[,] lattice, int n, int m)
+        {
+            if (n == lattice.GetLength(0) || m == lattice.GetLength(1))
+                return 0;
+            
+            var existingLatticeNumber = lattice[n, m];
+            if (existingLatticeNumber == Full)
+                return 0;
+
+            if (existingLatticeNumber >= 0)
+                return lattice[n, m];
+
+            if (n == lattice.GetLength(0) - 1 && m == lattice.GetLength(1) - 1)
+            {
+                lattice[n, m] = 1;
+            }
+            else
+            {
+                lattice[n, m] = GetLatticePaths(lattice, n + 1, m) + GetLatticePaths(lattice, n, m + 1);
+            }
+
+            return lattice[n, m];
+        }
+    }
+}
---
+++ Problem 064 - Minimum Path Sum/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Configuration;
+
+namespace Problem_064___Minimum_Path_Sum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var grid = new[,] {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+
+            Console.WriteLine(new Solution().MinPathSum(grid));
+            for (var row = 0; row < grid.GetLength(0); row++)
+            {
+                for (var col = 0; col < grid.GetLength(1); col++)
+                {
+                    var elem = grid[row, col];
+                    var c = elem.ToString();
+                    Console.Write(c + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public int MinPathSum(int[,] grid)
+        {
+            for (var row = 0; row < grid.GetLength(0); row++)
+            {
+                for (var col = 0; col < grid.GetLength(1); col++)
+                {
+                    grid[row, col] = ~grid[row, col];
+                }
+            }
+            if (grid.GetLength(0) == 0 || grid.GetLength(1) == 0)
+                return 0;
+            return MinPathSumHelper(grid, 0, 0);
+        }
+
+        public int MinPathSumHelper(int[,] grid, int row, int col)
+        {
+            if (row == grid.GetLength(0) || col == grid.GetLength(1))
+                return int.MaxValue;
+            if (row == grid.GetLength(0) - 1 && col == grid.GetLength(1) - 1)
+                return ~grid[row, col];
+            if (grid[row, col] >= 0) // nonnegative num => predetermined sum
+                return grid[row, col];
+            var additionalSum = -grid[row, col] - 1;
+
+            var bottomPath = MinPathSumHelper(grid, row + 1, col);
+            var rightPath = MinPathSumHelper(grid, row, col + 1);
+            var output = additionalSum + Math.Min(bottomPath, rightPath);
+
+            grid[row, col] = output;
+
+            return output;
+        }
+    }
+}
